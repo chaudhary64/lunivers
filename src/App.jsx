0fb7348c68f0db7ed2165601abd39f8c22118e3f
chaudhary64 <chiragchoudhary64@gmail.com>
2025-05-17 10:51:39 +0530

    feat: add Home component and integrate it into App; include home_intro.gif for background text

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Nav from "./Nav/Nav";
 import Cursor from "./Cursor/Cursor";
+import Home from "./Pages/Home/Home";
 
 const App = () => {
   return (
     <>
       <Nav />
       <Cursor />
+      <Home />
     </>
   );
 };
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,22 @@
+const Home = () => {
+  return (
+    <>
+      <div className="leading-none">
+        <p
+          style={{
+            backgroundImage: 'url("/images/home_intro.gif")',
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            fontSize: "clamp(25vw, 26.5vw, 28vw)",
+          }}
+          className="text-center text-transparent bg-clip-text bg-linear-to-r from-cyan-500 to-blue-500"
+        >
+          lunivers
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Home;
